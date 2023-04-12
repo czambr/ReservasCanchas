9034c2f61ea9ea677b04570af0941deea3ff0c83
czambr <cmzambra@espol.edu.ec>
2023-04-11 20:46:51 -0500

    Reserva Canchas - 2 View

--- ReservaCanchas/Reserva/views.py
+++ ReservaCanchas/Reserva/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-import datetime
 from django.http import HttpResponse
+from django.views .generic import TemplateView
+import datetime
 from .models import Cancha
 # Create your views here.
 
 def getInfoCanchaById (request, id_cancha):
     cancha = Cancha.objects.get(id=id_cancha)
-    print(cancha)
     result_layout = f"<h3>Cancha: {cancha.nombre}, Descripción: {cancha.descripcion}</h3>"
 
     return HttpResponse(result_layout)
+
+
+class MainView(TemplateView):
+    template_name = "main.html"
